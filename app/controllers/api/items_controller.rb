3322b0aee634e4176f250cbd5d509b86d8777cd6
Brianna Bell <bbell9563@gmail.com>
2020-04-13 10:21:52 -0600

    finished stubbing out actions in controller

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,10 +1,28 @@
 class Api::ItemsController < ApplicationController
+  before_action :set_item, only: [:show]
   def index
+    render json: Item.all
   end
 
   def show
+    render json: @item
   end
 
   def create
+    item = Item.new(item_params)
+    if item.save
+      render json: item
+    else
+      render json: {errors:item.errors}, status: :unproccessable_entity
+    end
   end
+
+  private
+    def set_item
+      @item = Item.find(params[:id])
+    end
+    
+    def item_params
+      params.require(:item).permit(:name, :image, :description, :likes)
+    end
 end
